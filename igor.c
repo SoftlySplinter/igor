c2be7c5db0cd469245d0b1a323e15f672d60df50
Alex Brown <alex@alexanderdbrown.com>
2014-04-08 10:55:18 +0100

    Chapter 6.

--- igor.c
+++ igor.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lib/mpc.h"
+
 #ifdef _WIN32
 #include <string.h>
 static char buffer[2048];
@@ -20,14 +22,38 @@ void add_history(char* unused) {}
 #endif // _WIN32
 
 int main(int argc, char** argv) {
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Igor     = mpc_new("igor");
+
+  mpca_lang(MPC_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      igor     : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Igor);
+
+
   puts("Igor Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exist\n");
+  puts("Press Ctrl+c to Exit\n");
 
   while(1) {
     char* input = readline("igor> ");
     add_history(input);
-    printf("%s\n", input);
+    mpc_result_t r;
+    if(mpc_parse("<stdin>", input, Igor, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Igor);
   return 0;
 }
